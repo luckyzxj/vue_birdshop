4565b62c5b52a37f9286ae3c238e9ae07cb94d64
zxj <17621160308@163.com>
2020-02-18 18:35:26 +0800

    birdshop

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+// vuex最核心的管理对象
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+import mutations from './mutations'
+import state from './state'
+import getters from './getters'
+import actions from './actions'
+
+Vue.use(Vuex)
+export default new Vuex.Store({
+  state,
+  mutations,
+  getters,
+  actions
+})
